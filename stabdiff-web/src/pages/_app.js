c3a54a889095479f6b2d674adb57812acf1d6f04
Batbayar <coder.batbayar@gmail.com>
2023-04-06 20:04:34 +0800

    Theme created.

--- stabdiff-web/src/pages/_app.js
+++ stabdiff-web/src/pages/_app.js
@@ -1,5 +1,10 @@
+import MainTheme from '@/theme/theme';
+import { ThemeProvider } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <ThemeProvider theme={MainTheme}>
+    <CssBaseline />
+    <Component {...pageProps} />
+  </ThemeProvider>
 }
---
+++ stabdiff-web/src/theme/theme.js
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+
+/**
+ * Main theme
+ */
+const MainTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+})
+
+export default MainTheme;
